8fe6f4ecdc508b4d8e7bbc4eb3d6cbe02ae5ddd5
Felipe Riboldi <feliperiboldimarques@gmail.com>
2019-12-28 12:25:20 -0300

    Exemplo de Renderização condicional

---
+++ exercicios/src/componentes/ParImpar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import If from './If';
+import Padrao from './../estilo/Padrao';
+
+function parOuImpar(num) {
+    // if(num % 2 == 0) {
+    //     return <Text style={Padrao.ex}>Par</Text>;
+    // } else {
+    //     <Text style={Padrao.ex}>Impar</Text>
+    // }
+
+    const v = num % 2 == 0 ? 'Par' : 'Impar';
+    return <Text style={Padrao.ex}>{v}</Text>
+}
+
+export default props =>
+    <View>
+        {parOuImpar(props.numero)}
+        {/* {
+            props.numero % 2 == 0
+            ? <Text style={Padrao.ex}>Par</Text>
+            : <Text style={Padrao.ex}>Impar</Text>
+        } */}
+    </View>
